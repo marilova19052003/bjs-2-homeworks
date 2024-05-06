88d455fbe8d4f5b473ffba6e46c94b9e1ede5ab4
Mariana <marilovamariana@gmail.com>
2024-05-06 22:37:52 +0300

    Четвёртое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,46 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+    this.subject = '';
+
+    // Метод для установки предмета
+    Student.prototype.setSubject = function(subjectName) {
+        this.subject = subjectName;
+    };
+
+    // Метод для добавления оценок
+    Student.prototype.addMarks = function(...marksToAdd) {
+        this.marks.push(...marksToAdd);
+    };
+
+    // Метод для вычисления среднего балла
+    Student.prototype.getAverage = function() {
+        if (!this.marks || this.marks.length === 0) {
+            return 0;
+        }
+        const sum = this.marks.reduce((total, mark) => total + mark, 0);
+        return sum / this.marks.length;
+    };
+
+    // Метод для исключения студента
+    Student.prototype.exclude = function(reason) {
+        delete this.subject;
+        delete this.marks;
+        this.excluded = reason;
+    };
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage()); // 0
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage()); // 4.5
+console.log(student1);
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+let student2 = new Student("Артём", "мужской", 25);
+student2.setSubject("Geometry");
+student2.exclude('плохая учёба');
+console.log(student2);
 
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
-}
