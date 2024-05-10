e245a97b4f5bd14228637f29282d2bcd1a133685
Mariana <marilovamariana@gmail.com>
2024-05-10 16:55:28 +0300

    Четвёртое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,45 +1,45 @@
-function createStudent(name, gender, age) {
-    const student = {
-        name: name,
-        gender: gender,
-        age: age,
-        marks: [],
-        subject: '',
+function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+    this.subject = '';
 
-        setSubject: function(subjectName) {
-            this.subject = subjectName;
-        },
-
-        addMarks: function(...marksToAdd) {
-            this.marks.push(...marksToAdd);
-        },
-
-        getAverage: function() {
-            if (!this.marks || this.marks.length === 0) {
-                return 0;
-            }
-            const sum = this.marks.reduce((total, mark) => total + mark, 0);
-            return sum / this.marks.length;
-        },
-
-        exclude: function(reason) {
-            delete this.subject;
-            delete this.marks;
-            this.excluded = reason;
-        }
+    
+    Student.prototype.setSubject = function(subjectName) {
+        this.subject = subjectName;
     };
 
-    return student;
+    
+    Student.prototype.addMarks = function(...marksToAdd) {
+        this.marks.push(...marksToAdd);
+    };
+
+   
+    Student.prototype.getAverage = function() {
+        if (!this.marks || this.marks.length === 0) {
+            return 0;
+        }
+        const sum = this.marks.reduce((total, mark) => total + mark, 0);
+        return sum / this.marks.length;
+    };
+
+   
+    Student.prototype.exclude = function(reason) {
+        this.subject = '';
+        this.marks = [];
+        this.excluded = reason;
+    };
 }
 
-let student1 = createStudent("Василиса", "женский", 19);
+let student1 = new Student("Василиса", "женский", 19);
 student1.setSubject("Algebra");
 console.log(student1.getAverage()); // 0
 student1.addMarks(4, 5, 4, 5);
 console.log(student1.getAverage()); // 4.5
 console.log(student1);
 
-let student2 = createStudent("Артём", "мужской", 25);
+let student2 = new Student("Артём", "мужской", 25);
 student2.setSubject("Geometry");
 student2.exclude('плохая учёба');
 console.log(student2);
