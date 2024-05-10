213806f81be045122a5ef54c3bbd5fc9405c8cd7
Mariana <marilovamariana@gmail.com>
2024-05-10 17:26:32 +0300

    Четвёртое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,32 +1,42 @@
-function Student(name) {
+function Student(name, gender, age) {
     this.name = name;
-    this.subject = 'Math';
-    this.excluded = false;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+    this.subject = '';
 }
 
-Student.prototype.exclude = function(reason) {
-    if (!this.excluded) {
-        delete this.subject;
-        delete this.marks;
-        this.excluded = reason;
-    }
+// Методы добавляются через прототип
+Student.prototype.setSubject = function(subjectName) {
+    this.subject = subjectName;
 };
 
-Student.prototype.getAverage = function(marks) {
-    if (!marks || marks.length === 0 || this.excluded) {
+Student.prototype.addMarks = function(...marksToAdd) {
+    this.marks.push(...marksToAdd);
+};
+
+Student.prototype.getAverage = function() {
+    if (!this.marks || this.marks.length === 0) {
         return 0;
     }
-    const sum = marks.reduce((total, mark) => total + mark, 0);
-    return sum / marks.length;
+    const sum = this.marks.reduce((total, mark) => total + mark, 0);
+    return sum / this.marks.length;
 };
 
-// Пример использования
-const student1 = new Student('Alice');
-const student2 = new Student('Bob');
+Student.prototype.exclude = function(reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+};
 
-const aliceMarks = [4, 5, 3];
-const bobMarks = [5, 5, 4];
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage()); // 0
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage()); // 4.5
+console.log(student1);
 
-console.log(student1.getAverage(aliceMarks)); // Выведет средний балл для Алисы
-student1.exclude('Poor attendance');
-console.log(student1.getAverage([5, 4])); // Выведет 0, так как студент отчислен и у него нет оценок
+let student2 = new Student("Артём", "мужской", 25);
+student2.setSubject("Geometry");
+student2.exclude('плохая учёба');
+console.log(student2);
