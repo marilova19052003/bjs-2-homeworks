f2dd4cebc8787549059c44debeb4c907c1b6d568
Mariana <marilovamariana@gmail.com>
2024-05-10 16:49:48 +0300

    Четвёртое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -2,36 +2,40 @@ function Student(name, gender, age) {
     this.name = name;
     this.gender = gender;
     this.age = age;
-    this.marks = [];
     this.subject = '';
-
-    // Метод для установки предмета
-    Student.prototype.setSubject = function(subjectName) {
-        this.subject = subjectName;
-    };
-
-    // Метод для добавления оценок
-    Student.prototype.addMarks = function(...marksToAdd) {
-        this.marks.push(...marksToAdd);
-    };
-
-    // Метод для вычисления среднего балла
-    Student.prototype.getAverage = function() {
-        if (!this.marks || this.marks.length === 0) {
-            return 0;
-        }
-        const sum = this.marks.reduce((total, mark) => total + mark, 0);
-        return sum / this.marks.length;
-    };
-
-    // Метод для исключения студента
-    Student.prototype.exclude = function(reason) {
-        delete this.subject;
-        delete this.marks;
-        this.excluded = reason;
-    };
+    this.marks = [];
+    this.excluded = '';
 }
 
+Student.prototype.setSubject = function(subjectName) {
+    this.subject = subjectName;
+};
+
+Student.prototype.addMarks = function(...marksToAdd) {
+    if (this.excluded) {
+        console.log("Студент не может получать оценки.");
+        return;
+    }
+    
+    this.marks.push(...marksToAdd);
+};
+
+Student.prototype.getAverage = function() {
+    if (!this.marks || this.marks.length === 0) {
+        return 0;
+    }
+    
+    const sum = this.marks.reduce((total, mark) => total + mark, 0);
+    return sum / this.marks.length;
+};
+
+Student.prototype.exclude = function(reason) {
+    this.subject = '';
+    this.marks = [];
+    this.excluded = reason;
+};
+
+// Пример использования
 let student1 = new Student("Василиса", "женский", 19);
 student1.setSubject("Algebra");
 console.log(student1.getAverage()); // 0
