3a18685631378c7902f60710f20d121d9a2735bd
Mariana <marilovamariana@gmail.com>
2024-05-10 17:10:20 +0300

    Четвёртое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,50 +1,28 @@
-function Student(name, gender, age) {
+function Student(name) {
     this.name = name;
-    this.gender = gender;
-    this.age = age;
-    this.marks = [];
-    this.subject = '';
+    this.subject = 'Math';
+    this.excluded = false;
 }
-
-// Методы добавляются через прототип
-Student.prototype.setSubject = function(subjectName) {
-    this.subject = subjectName;
-};
-
-Student.prototype.addMarks = function(...marksToAdd) {
-    this.marks.push(...marksToAdd);
-};
-
-Student.prototype.getAverage = function() {
-    if (!this.marks || this.marks.length === 0) {
-        return 0;
-    }
-    const sum = this.marks.reduce((total, mark) => total + mark, 0);
-    return sum / this.marks.length;
-};
 
 Student.prototype.exclude = function(reason) {
     delete this.subject;
-    delete this.marks;
     this.excluded = reason;
 };
 
-Student.prototype.getAverage = function() {
-    if (!this.marks || this.marks.length === 0 || this.excluded) {
+Student.prototype.getAverage = function(marks) {
+    if (!marks || marks.length === 0 || this.excluded) {
         return 0;
     }
-    const sum = this.marks.reduce((total, mark) => total + mark, 0);
-    return sum / this.marks.length;
+    const sum = marks.reduce((total, mark) => total + mark, 0);
+    return sum / marks.length;
 };
 
-let student1 = new Student("Василиса", "женский", 19);
-student1.setSubject("Algebra");
-console.log(student1.getAverage()); // 0
-student1.addMarks(4, 5, 4, 5);
-console.log(student1.getAverage()); // 4.5
-console.log(student1);
+// Пример использования
+const student1 = new Student('Alice');
+const student2 = new Student('Bob');
 
-let student2 = new Student("Артём", "мужской", 25);
-student2.setSubject("Geometry");
-student2.exclude('плохая учёба');
-console.log(student2);
+const aliceMarks = [4, 5, 3];
+const bobMarks = [5, 5, 4];
+
+console.log(student1.getAverage(aliceMarks)); // Выведет средний балл для Алисы
+console.log(student2.getAverage(bobMarks)); // Выведет средний балл для Боба
