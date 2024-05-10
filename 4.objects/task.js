202eeb6218503cd6cca84de0bc2794265a906a05
Mariana <marilovamariana@gmail.com>
2024-05-10 16:38:35 +0300

    Четвёртое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -2,7 +2,6 @@ function Student(name, gender, age) {
     this.name = name;
     this.gender = gender;
     this.age = age;
-    this.marks = [];
     this.subject = '';
 
     
@@ -11,23 +10,13 @@ function Student(name, gender, age) {
     };
 
     
-    Student.prototype.addMarks = function(...marksToAdd) {
-        this.marks.push(...marksToAdd);
-    };
-
-   
     Student.prototype.getAverage = function() {
-        if (!this.marks || this.marks.length === 0) {
-            return 0;
-        }
-        const sum = this.marks.reduce((total, mark) => total + mark, 0);
-        return sum / this.marks.length;
+        return 0; // Возвращаем 0, так как нет оценок
     };
 
-   
+    
     Student.prototype.exclude = function(reason) {
         this.subject = '';
-        this.marks = [];
         this.excluded = reason;
     };
 }
@@ -35,8 +24,6 @@ function Student(name, gender, age) {
 let student1 = new Student("Василиса", "женский", 19);
 student1.setSubject("Algebra");
 console.log(student1.getAverage()); // 0
-student1.add(4, 5, 4, 5);
-console.log(student1.getAverage()); // 4.5
 console.log(student1);
 
 let student2 = new Student("Артём", "мужской", 25);
