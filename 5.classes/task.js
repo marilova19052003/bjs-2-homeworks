76b02a1e69de8ab2007a257c4245a24d94abf3a2
Mariana <marilovamariana@gmail.com>
2024-06-17 23:50:16 +0300

    Пятое задание полностью готово

---
+++ 5.classes/task.js
@@ -0,0 +1,64 @@
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+      this.name = name;
+      this.releaseDate = releaseDate;
+      this.pagesCount = pagesCount;
+      this.state = 100;
+      this.type = null;
+    }
+  
+    fix() {
+      this.state *= 1.5;
+    }
+  
+    set state(newState) {
+      if (newState < 0) {
+        this._state = 0;
+      } else if (newState > 100) {
+        this._state = 100;
+      } else {
+        this._state = newState;
+      }
+    }
+  
+    get state() {
+      return this._state;
+    }
+  }
+
+  class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+      super(name, releaseDate, pagesCount);
+      this.type = "magazine";
+    }
+  }
+  
+  class Book extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount, author) {
+      super(name, releaseDate, pagesCount);
+      this.type = "book";
+      this.author = author;
+    }
+  }
+  
+  class Library {
+    constructor(name) {
+      this.name = name;
+      this.books = [];
+    }
+  
+    addBook(book) {
+      if (book.state > 30) {
+        this.books.push(book);
+      }
+    }
+  
+    findBookBy(type, value) {
+      for (let book of this.books) {
+        if (book[type] === value) {
+          return book;
+        }
+      }
+      return null;
+    }
+  } 
