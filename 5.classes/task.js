4908b3ae2ba9217ea2816c77d1319ac793177596
Mariana <marilovamariana@gmail.com>
2024-06-18 11:02:36 +0300

    Пятое задание полностью готово

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,44 +1,74 @@
-class PrintPublication {
-    constructor(title, author, year) {
-      this.title = title;
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+      this.name = name;
+      this.releaseDate = releaseDate;
+      this.pagesCount = pagesCount;
+      this.state = 100;
+      this.type = null;
+    }
+  
+    fix() {
+      this.state *= 1.5;
+    }
+  
+    set state(newState) {
+      if (newState < 0) {
+        this._state = 0;
+      } else if (newState > 100) {
+        this._state = 100;
+      } else {
+        this._state = newState;
+      }
+    }
+  
+    get state() {
+      return this._state;
+    }
+  }
+  
+  class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+      super(name, releaseDate, pagesCount);
+      this.type = "magazine";
+    }
+  }
+  
+  class Book extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount, author) {
+      super(name, releaseDate, pagesCount);
+      this.type = "book";
       this.author = author;
-      this.year = year;
-    }
-  }
-  
-  class Book extends PrintPublication {
-    constructor(title, author, year, genre) {
-      super(title, author, year);
-      this.genre = genre;
-    }
-  }
-  
-  class Magazine extends PrintPublication {
-    constructor(title, author, year, category) {
-      super(title, author, year);
-      this.category = category;
     }
   }
   
   class Library {
-    constructor() {
+    constructor(name) {
+      this.name = name;
       this.books = [];
-      this.magazines = [];
     }
   
     addBook(book) {
-      this.books.push(book);
+      if (book.state > 30) {
+        this.books.push(book);
+      }
     }
   
-    addMagazine(magazine) {
-      this.magazines.push(magazine);
+    findBookBy(type, value) {
+      for (let book of this.books) {
+        if (book[type] === value) {
+          return book;
+        }
+      }
+      return null;
     }
   }
   
-  // Пример использования:
-  const library = new Library();
-  const book1 = new Book("Преступление и наказание", "Федор Достоевский", 1866, "роман");
-  const magazine1 = new Magazine("National Geographic", "Various", 1888, "наука");
+  const magazine1 = new Magazine("Magazine1", "2021-01-01", 100);
+  const book1 = new Book("Book1", "2020-01-01", 200, "Author1");
   
+  const library = new Library("My Library");
+  library.addBook(magazine1);
   library.addBook(book1);
-  library.addMagazine(magazine1);
+  
+  const foundBook = library.findBookBy("author", "Author1");
+  console.log(foundBook);
